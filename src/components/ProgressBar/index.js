2c71826397e502cf7281a10d7a53a32a42070c0e
TDH55 <taylorhoward1@wustl.edu>
2021-06-07 17:55:59 -0500

    Added directional options

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,14 +2,59 @@ import React, { useEffect, useRef } from 'react'
 import { View, StyleSheet, Animated } from 'react-native'
 
 const ProgressBar = props => {
-
-  const { progressValue, maxValue, backgroundColor, progressColor, animationSpeed, animationBounciness, rounding, _height: height } = props
+  // console.log(props)
+  const { progressValue, maxValue, backgroundColor, progressColor, animationSpeed, animationBounciness, rounding, _height: height, _width, direction } = props
   let animation = useRef(new Animated.Value(0))
-  const width = animation.current.interpolate({
-    inputRange: [0, maxValue],
-    outputRange: ["0%", "100%"],
-    extrapolate: "clamp"
-  })
+  let width
+  let innerHeight
+  let alignSelf
+  let flexDirection
+  // console.log(direction)
+  switch (direction) {
+    case 0:
+      flexDirection = "row"
+      // alignSelf = 'flex-start'
+      width = animation.current.interpolate({
+        inputRange: [0, maxValue],
+        outputRange: ["0%", "100%"],
+        extrapolate: "clamp"
+      })
+      innerHeight = height
+      break;
+    case 1:
+      flexDirection = "row-reverse"
+      // alignSelf = 'flex-end'
+      width = animation.current.interpolate({
+        inputRange: [0, maxValue],
+        outputRange: ["0%", "100%"],
+        extrapolate: "clamp"
+      })
+      innerHeight = height
+      break;
+    case 2:
+      flexDirection = "column"
+      innerHeight = animation.current.interpolate({
+        inputRange: [0, maxValue],
+        outputRange: ["0%", "100%"],
+        extrapolate: "clamp"
+      })
+      width = _width
+      break;
+    case 3:
+      flexDirection = "column-reverse"
+      innerHeight = animation.current.interpolate({
+        inputRange: [0, maxValue],
+        outputRange: ["0%", "100%"],
+        extrapolate: "clamp"
+      })
+      width = _width
+      break;
+    // default:
+    //   console.log("This shouldn't happen")
+    //   break;
+  }
+
+  
   const borderRadius = (rounding / 100) * (height / 2)
 
   useEffect(() => {
@@ -22,9 +67,11 @@ const ProgressBar = props => {
     }).start()
   }, [progressValue])
 
+  
+
   return (
-    <View style={[styles.wrapper, {backgroundColor: backgroundColor, height, borderRadius}]}>
-      <Animated.View style={[StyleSheet.progress], {backgroundColor: progressColor, height, width}} />
+    <View style={[styles.wrapper, {backgroundColor: backgroundColor, height, borderRadius, flexDirection, overflow: 'hidden'}]}>
+      <Animated.View style={[StyleSheet.progress], {backgroundColor: progressColor, height: innerHeight, width, borderRadius}} />
     </View>
   )
 }
