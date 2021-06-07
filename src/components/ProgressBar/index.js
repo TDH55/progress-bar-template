59e8763cb51c5c3049907c635a1c1883a9cb306f
TDH55 <taylorhoward1@wustl.edu>
2021-06-07 13:46:52 -0500

    animation v1

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,18 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { View, StyleSheet, Animated } from 'react-native'
 
 const ProgressBar = props => {
 
   console.log(props)
   const { progressValue, maxValue, color, backgroundColor, rounding, _width, _height } = props
-  const width = (progressValue / maxValue) * _width
   const height = _height
+  let animation = useRef(new Animated.Value(0))
+  const width = animation.current.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+    extrapolate: "clamp"
+  })
   const borderRadius = (rounding / 100) * (height / 2)
+
+  useEffect(() => {
+    Animated.timing(animation.current, {
+      toValue: progressValue,
+      duration: 100
+    }).start()
+  }, [progressValue])
 
   return (
     <View style={[styles.wrapper, {backgroundColor: color, height, borderRadius}]}>
-      <View style={[styles.progress,{ backgroundColor: backgroundColor, width, height, borderRadius}]} >
-      </View>
+      <Animated.View style={[StyleSheet.progress], {backgroundColor: backgroundColor, height, width}} />
     </View>
   )
 }
