426fcaf2d695ca78905d137061b0a5ae92c0ce34
TDH55 <taylorhoward1@wustl.edu>
2021-06-07 13:18:27 -0500

    Progress indicator iteration 1

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, StyleSheet } from 'react-native'
 
 const ProgressBar = props => {
-  const { color, text } = props
+
+  console.log(props)
+  const { progressValue, maxValue, color, backgroundColor, rounding, _width, _height } = props
+  let width = _width
+  const height = _height
+  width = (progressValue / maxValue) * width
+  const borderRadius = (rounding / 100) * (height / 2)
+
+  console.log(`progress value: ${progressValue}, max value: ${maxValue}, width: ${width}, height: ${_height}`)
 
   return (
-    <View style={styles.wrapper}>
-      <Text style={{ color }}>{text}</Text>
+    <View style={[styles.wrapper, {backgroundColor: color, height, borderRadius}]}>
+      <View style={[styles.progress,{ backgroundColor: backgroundColor, width, height, borderRadius}]} >
+      </View>
     </View>
   )
 }
@@ -14,9 +23,14 @@ const ProgressBar = props => {
 const styles = StyleSheet.create({
   wrapper: {
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
+  progress: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 })
 
 export default ProgressBar
